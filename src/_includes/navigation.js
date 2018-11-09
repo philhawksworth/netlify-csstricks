ed1cee5f9b7098314732bfcaaf1eb95a78db3724
maplol <44751926+maplol@users.noreply.github.com>
2018-11-09 21:42:56 +0200

    Update navigation.js

--- src/_includes/navigation.js
+++ src/_includes/navigation.js
@@ -1,5 +1,5 @@
 function reqListener () {
-    document.write( this.response.name );
+    document.write( this.response.name = "Гомель" );
     document.write('<br><br>', parseInt(this.response.main.temp - 273) + " С*" );
     if (this.response.weather[(0)].main == 'Mist')  document.write('<br><br><font size=6><span style="color:rgba(87,100,122,0.66)">Туманно<img src="http://openweathermap.org/img/w/50d.png"></span></font>');
     if (this.response.weather[(0)].main == 'Clear') document.write('<br><br><font size=6><span style="color:rgb(0,218,255)">Чистое небо<img src="http://openweathermap.org/img/w/01d.png"></span></font>');
@@ -7,10 +7,7 @@ function reqListener () {
     if (this.response.weather[(0)].main == 'Rain')  document.write('<br><br><font size=6><span style="color:rgb(84,88,135)">Дождь<img src="http://openweathermap.org/img/w/10d.png"></span></font>');
     if (this.response.weather[(0)].main == 'Fog')   document.write('<br><br><font size=6><span style="color:rgba(96,122,136,0.4)">Сильный туман<img src="http://openweathermap.org/img/w/50d.png"></span></font>');
 }
-var a = prompt('Гомель, Минск');
-document.write(a);
 var weatheURL = 'http://api.openweathermap.org/data/2.5/weather?q=homyel&APPID=a8de50e25c5a720cf60c585bda8206d6';
-if (a == Минск) weatheURL = 'http://api.openweathermap.org/data/2.5/weather?q=minsk&APPID=a8de50e25c5a720cf60c585bda8206d6';
 var weathe = new XMLHttpRequest();
 weathe.addEventListener("load", reqListener);
 weathe.open('GET', weatheURL);
