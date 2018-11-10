f45b96a3e5384ef0d24decf238af6c1d5e4e683d
maplol <44751926+maplol@users.noreply.github.com>
2018-11-10 10:38:38 +0200

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -1,11 +1,11 @@
 function reqListener () {
     document.write( this.response.name = "Гомель" );
-    document.write('<br><br>', parseInt(this.response.main.temp - 273) + " С*" );
+    document.write( parseInt(this.response.main.temp - 273) + " С*" );
     if (this.response.weather[(0)].main === 'Mist')  document.write('Туманно');
     if (this.response.weather[(0)].main === 'Clear') document.write('Чистое небо');
     if (this.response.weather[(0)].main === 'Clouds')document.write('Облачнo');
     if (this.response.weather[(0)].main === 'Rain')  document.write('Дождь');
-    if (this.response.weather[(0)].main === Fog)   document.write('Сильный туман');
+    if (this.response.weather[(0)].main === 'Fog')   document.write('Сильный туман');
 }
 var weatheURL = 'https://api.openweathermap.org/data/2.5/weather?q=homyel&APPID=a8de50e25c5a720cf60c585bda8206d6';
 var weathe = new XMLHttpRequest();
