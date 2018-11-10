b86c9b948c9147f55ed3b3d9f0381b253b3843da
maplol <44751926+maplol@users.noreply.github.com>
2018-11-10 10:29:41 +0200

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -7,7 +7,7 @@ function reqListener () {
     if (this.response.weather[(0)].main == 'Rain')  document.write('<br><br><font size=6><span style="color:rgb(84,88,135)">Дождь<img src="http://openweathermap.org/img/w/10d.png"></span></font>');
     if (this.response.weather[(0)].main == 'Fog')   document.write('<br><br><font size=6><span style="color:rgba(96,122,136,0.4)">Сильный туман<img src="http://openweathermap.org/img/w/50d.png"></span></font>');
 }
-var weatheURL = 'http://api.openweathermap.org/data/2.5/weather?q=homyel&APPID=a8de50e25c5a720cf60c585bda8206d6';
+var weatheURL = 'https://api.openweathermap.org/data/2.5/weather?q=homyel&APPID=a8de50e25c5a720cf60c585bda8206d6';
 var weathe = new XMLHttpRequest();
 weathe.addEventListener("load", reqListener);
 weathe.open('GET', weatheURL);
