4dad81ef315d881b717f320daf1cb0e89ba742e2
maplol <44751926+maplol@users.noreply.github.com>
2018-11-12 20:12:02 +0200

    Create minsk.js

---
+++ src/minsk.js
@@ -0,0 +1,27 @@
+var a = ('<style>\n' +
+    '   body {\n' +
+    '    background: url(https://media3.giphy.com/media/VxbvpfaTTo3le/giphy.gif) repeat;\n' +
+    '    -moz-background-size: 100%; \n' +
+    '    -webkit-background-size: 100%; \n' +
+    '    -o-background-size: 100%; \n' +
+    '    background-size: 100%; \n' +
+    '   }\n' +
+    '  </style>');
+
+
+function reqListener () {
+    document.write(a,'<font face=\'Calibri\'><center><b>');
+    document.write(this.response.name = "<font size=7 >Минск");
+    document.write('<br><br><font size=5 >',parseInt(this.response.main.temp - 273) + ' °C');
+    if      (this.response.weather[(0)].main == 'Mist')  document.write('<br><br><font size=6 ><span style="color:rgba(125,126,134,0.66)">Туманно</span>');
+    else if (this.response.weather[(0)].main == 'Clear') document.write('<br><br><font size=6 ><span style="color:rgb(0,218,255)">Чистое небо</span>');
+    else if (this.response.weather[(0)].main == 'Clouds')document.write('<br><br><font size=6 ><span style="color:rgb(124,188,182)">Облачно</span>');
+    else if (this.response.weather[(0)].main == 'Rain')  document.write('<br><br><font size=6 ><span style="color:rgb(84,88,135)">Дождь</span>');
+    else if (this.response.weather[(0)].main == 'Fog')   document.write('<br><br><font size=6 ><span style="color:rgba(41,41,41,0.54)">Сильный туман</span></font></center></b>');
+}
+var weatheURL = 'https://api.openweathermap.org/data/2.5/weather?q=minsk&APPID=a8de50e25c5a720cf60c585bda8206d6';
+var weathe = new XMLHttpRequest();
+weathe.addEventListener("load", reqListener);
+weathe.open('GET', weatheURL, true);
+weathe.responseType = 'json';
+weathe.send('');
